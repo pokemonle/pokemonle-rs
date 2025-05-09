a6c81e30257374066cb8ce971863cc23f820f694
incubator4 <aries0robin@gmail.com>
2025-05-10 01:08:12 +0800

    fix: language official bool value

--- migrations/2025-05-01-063857_create_generations_and_languages/up.sql
+++ migrations/2025-05-01-063857_create_generations_and_languages/up.sql
@@ -28,16 +28,16 @@ INSERT INTO "generations" ("id", "main_region_id", "identifier") VALUES
 ('9', '10', 'generation-ix');
 
 INSERT INTO "languages" ("id", "iso639", "iso3166", "identifier", "official", "order") VALUES
-('1', 'ja', 'jp', 'ja-Hrkt', 'true', '1'),
-('2', 'ja', 'jp', 'roomaji', 'true', '3'),
-('3', 'ko', 'kr', 'ko', 'true', '4'),
-('4', 'zh', 'cn', 'zh-Hant', 'true', '5'),
-('5', 'fr', 'fr', 'fr', 'true', '8'),
-('6', 'de', 'de', 'de', 'true', '9'),
-('7', 'es', 'es', 'es', 'true', '10'),
-('8', 'it', 'it', 'it', 'true', '11'),
-('9', 'en', 'us', 'en', 'true', '7'),
-('10', 'cs', 'cz', 'cs', '0', '12'),
-('11', 'ja', 'jp', 'ja', 'true', '2'),
-('12', 'zh', 'cn', 'zh-Hans', 'true', '6'),
-('13', 'pt-BR', 'br', 'pt-BR', 'false', '13');
+('1', 'ja', 'jp', 'ja-Hrkt', '1', '1'),
+('2', 'ja', 'jp', 'roomaji', '1', '3'),
+('3', 'ko', 'kr', 'ko', '1', '4'),
+('4', 'zh', 'cn', 'zh-Hant', '1', '5'),
+('5', 'fr', 'fr', 'fr', '1', '8'),
+('6', 'de', 'de', 'de', '1', '9'),
+('7', 'es', 'es', 'es', '1', '10'),
+('8', 'it', 'it', 'it', '1', '11'),
+('9', 'en', 'us', 'en', '1', '7'),
+('10', 'cs', 'cz', 'cs', '1', '12'),
+('11', 'ja', 'jp', 'ja', '1', '2'),
+('12', 'zh', 'cn', 'zh-Hans', '1', '6'),
+('13', 'pt-BR', 'br', 'pt-BR', '0', '13');
