71a4c8049422c8cb0bfb841c17537b783408d02e
incubator4 <aries0robin@gmail.com>
2025-05-10 13:41:25 +0800

    fix:  languaged resource route path prefix with languges

--- pokemonle-http/src/v1/language.rs
+++ pokemonle-http/src/v1/language.rs
@@ -179,15 +179,17 @@ pub fn routers() -> ApiRouter<AppState> {
             api_routers::<Version, _, _>(|state| state.pool.version()),
         );
 
-    ApiRouter::new().nest("/{lang}", sub_routers).api_route(
-        "/local-languages",
-        get_with(get_local_lanuages, |op| {
-            op.tag("language")
-                .response_with::<200, Json<PaginatedResource<LanguageName>>, _>(|o| {
-                    o.description("Get all local languages")
-                })
-        }),
-    )
+    ApiRouter::new()
+        .nest("/languages/{lang}", sub_routers)
+        .api_route(
+            "/local-languages",
+            get_with(get_local_lanuages, |op| {
+                op.tag("language")
+                    .response_with::<200, Json<PaginatedResource<LanguageName>>, _>(|o| {
+                        o.description("Get all local languages")
+                    })
+            }),
+        )
 }
 
 async fn get_local_lanuages(State(state): State<AppState>) -> impl IntoApiResponse {
